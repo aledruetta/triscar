04527b5f0dde1564a86c7bb6faf14e3f7e2b9e0f
Alejandro Druetta <aledruetta@gmail.com>
2018-10-03 17:04:42 -0300

    Forward and brake by obstacle

--- triscar.ino
+++ triscar.ino
@@ -3,6 +3,9 @@
 
 #define ACCEL 1
 #define MAX_SPEED 255
+#define TRIGGER_PIN A1
+#define ECHO_PIN A0
+#define MAX_DISTANCE 200
 
 AF_DCMotor motor[] = {
   AF_DCMotor(1),          // front right
@@ -11,79 +14,14 @@ AF_DCMotor motor[] = {
   AF_DCMotor(4)           // back right
 };
 
-#define TRIGGER_PIN A1
-#define ECHO_PIN A0
-#define MAX_DISTANCE 100
+uint8_t speed[] = {0, 0, 0, 0};
+uint8_t state = RELEASE;
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+unsigned long distance;
 
-uint8_t state = BRAKE;
 bool debug = true;
 
-// move forward
-void forward(uint8_t accel) {
-  uint8_t i, j;
-
-  if (state != FORWARD) {
-    if (state == BACKWARD) brake(4 * ACCEL);
-    for (i=0; i<4; i++) {
-      motor[i].run(FORWARD);
-    }
-
-    if (debug) Serial.println("Forward...");
-
-    for (i=0; i<MAX_SPEED-accel; i+=accel) {
-      for (j=0; j<4; j++) {
-        motor[j].setSpeed(i);
-        delay(10);
-      }
-    }
-
-    state = FORWARD;
-  }
-}
-
-// move backward
-void backward(uint8_t accel) {
-  uint8_t i, j;
-
-  if (state != BACKWARD) {
-    if (state == FORWARD) brake(4 * ACCEL);
-    for (i=0; i<4; i++) {
-      motor[i].run(BACKWARD);
-    }
-
-    if (debug) Serial.println("Backward...");
-
-    for (i=0; i<MAX_SPEED-accel; i+=accel) {
-      for (j=0; j<4; j++) {
-        motor[j].setSpeed(i);
-        delay(10);
-      }
-    }
-
-    state = BACKWARD;
-  }
-}
-
-// brake
-void brake(uint8_t accel) {
-  uint8_t i, j;
-
-  if (state != BRAKE || state != RELEASE) {
-
-    if (debug) Serial.println("Brake...");
-
-    for (i=MAX_SPEED; i>0+accel; i-=accel) {
-      for (j=0; j<4; j++) {
-        motor[j].setSpeed(i);
-        delay(10);
-      }
-    }
-
-    state = BRAKE;
-  }
-}
 
 // release motors
 void release() {
@@ -93,12 +31,18 @@ void release() {
 
     if (debug) Serial.println("Release...");
 
-    for (i=0; i<4; i++) {
-      motor[i].run(state);
-    }
-
     state = RELEASE;
+    run_state(state);
+
     delay(1000);
+  }
+}
+
+void run_state(uint8_t state) {
+  uint8_t i;
+
+  for (i=0; i<4; i++) {
+    motor[i].run(state);
   }
 }
 
@@ -117,12 +61,45 @@ void setup() {
 
 void loop() {
 
-  //forward(ACCEL);
-  //backward(ACCEL);
-  delay(50);
-  Serial.print("Ping: ");
-  Serial.print(sonar.ping_cm());
-  Serial.println(" cm");
+  uint8_t i;
 
-  //release();
+  distance = sonar.ping_cm();
+  if (debug) {
+    Serial.print(distance);
+    Serial.print(" ");
+    Serial.print(state);
+    Serial.print(" ");
+    Serial.println(speed[0]);
+  }
+
+  if (state == RELEASE && distance > 5 && distance < 25) {
+    state = FORWARD;
+    run_state(state);
+    delay(1000);
+  } else if (state == FORWARD && distance > 0 && distance < 40) {
+    state = BRAKE;
+    run_state(state);
+  }
+
+  for (i=0; i<4; i++) {
+    switch (state) {
+      case FORWARD || BACKWARD:
+        if (speed[i] < MAX_SPEED) {
+          speed[i]++;
+          delay(10);
+        }
+        break;
+      case BRAKE:
+        if (speed[i] >= 2) {
+          speed[i] = speed[i] - 2;
+          delay(1);
+        }
+        break;
+      default:
+        break;
+    }
+
+    motor[i].setSpeed(speed[i]);
+  }
+
 }
