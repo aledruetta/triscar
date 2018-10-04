6a8ef5526f85500bc17579bf2ab00985404120fb
Alejandro Druetta <aledruetta@gmail.com>
2018-10-04 13:09:55 -0300

    Avoiding obstacles

--- triscar.ino
+++ triscar.ino
@@ -1,8 +1,19 @@
 #include <AFMotor.h>
 #include <NewPing.h>
 
-#define ACCEL 1
+#define STOP 0
+//FORWARD 1
+//BACKWARD 2
+//BRAKE 3
+//RELEASE 4
+#define AVOID 5
+
+#define NORMAL_ACCEL 1
+#define BRAKE_ACCEL 4
+#define AVOID_ACCEL 4
 #define MAX_SPEED 255
+
+// Ultrassom
 #define TRIGGER_PIN A1
 #define ECHO_PIN A0
 #define MAX_DISTANCE 200
@@ -14,92 +25,133 @@ AF_DCMotor motor[] = {
   AF_DCMotor(4)           // back right
 };
 
-uint8_t speed[] = {0, 0, 0, 0};
+uint8_t motor_speed[4];
 uint8_t state = RELEASE;
+bool obstacle = false;
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 unsigned long distance;
 
 bool debug = true;
 
+void avoid_obstacle() {
+  Serial.println("Avoiding");
+  uint8_t i, j;
 
-// release motors
-void release() {
-  uint8_t i;
+  motor[0].run(FORWARD);
+  motor[1].run(BACKWARD);
+  motor[2].run(BACKWARD);
+  motor[3].run(FORWARD);
 
-  if (state != RELEASE) {
-
-    if (debug) Serial.println("Release...");
-
-    state = RELEASE;
-    run_state(state);
-
-    delay(1000);
+  for (j=0; j<MAX_SPEED - AVOID_ACCEL; j+=AVOID_ACCEL) {
+    for (i=0; i<4; i++) {
+      motor_speed[i] = j;
+      motor[i].setSpeed(motor_speed[i]);
+      delay(10);
+    }
   }
-}
 
-void run_state(uint8_t state) {
-  uint8_t i;
+  for (j=MAX_SPEED; j>AVOID_ACCEL; j-=AVOID_ACCEL) {
+    for (i=0; i<4; i++) {
+      motor[i].setSpeed(j);
+      delay(10);
+    }
+  }
 
   for (i=0; i<4; i++) {
-    motor[i].run(state);
+    motor_speed[i] = 0;
+    motor[i].run(FORWARD);
   }
+
+  state = STOP;
 }
 
 void setup() {
   uint8_t i;
 
-  Serial.begin(115200);           // set up Serial library at 9600 bps
-  Serial.println("Motor test!");
+  Serial.begin(115200);
 
   for (i=0; i<4; i++) {
-    motor[i].setSpeed(200);
+    motor_speed[i] = 0;
+    motor[i].setSpeed(motor_speed[i]);
+    motor[i].run(state);
   }
 
-  release();
+  delay(1000);
 }
 
 void loop() {
-
-  uint8_t i;
+  uint8_t i, j;
 
   distance = sonar.ping_cm();
+  obstacle = (distance > 0 && distance < 40) ? true : false;
+
   if (debug) {
-    Serial.print(distance);
-    Serial.print(" ");
+    Serial.print("state: ");
     Serial.print(state);
-    Serial.print(" ");
-    Serial.println(speed[0]);
+    Serial.print(" obstacle? ");
+    Serial.print(obstacle);
+    Serial.print(" distance: ");
+    Serial.print(distance);
+    Serial.print(" speed: ");
+    for (i=0; i<4; i++) {
+      Serial.print(motor_speed[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
   }
 
-  if (state == RELEASE && distance > 5 && distance < 25) {
-    state = FORWARD;
-    run_state(state);
-    delay(1000);
-  } else if (state == FORWARD && distance > 0 && distance < 40) {
-    state = BRAKE;
-    run_state(state);
-  }
-
-  for (i=0; i<4; i++) {
+  if (obstacle) {
     switch (state) {
-      case FORWARD || BACKWARD:
-        if (speed[i] < MAX_SPEED) {
-          speed[i]++;
-          delay(10);
+      case RELEASE:
+        if (distance > 5 && distance < 15) {
+          delay(1000);
+          state = FORWARD;
+
+          for (i=0; i<4; i++) {
+            motor[i].run(state);
+          }
         }
         break;
-      case BRAKE:
-        if (speed[i] >= 2) {
-          speed[i] = speed[i] - 2;
-          delay(1);
-        }
+      case FORWARD:
+        state = BRAKE;
         break;
-      default:
+      case STOP:
+        state = AVOID;
         break;
     }
-
-    motor[i].setSpeed(speed[i]);
+  } else if (state == STOP) {
+    state = FORWARD;
   }
+
+  switch (state) {
+    case FORWARD:
+      for (i=0; i<4; i++) {
+        if (motor_speed[i] < MAX_SPEED-NORMAL_ACCEL) {
+          motor_speed[i] += NORMAL_ACCEL;
+        } else {
+          motor_speed[i] = MAX_SPEED;
+        }
+        motor[i].setSpeed(motor_speed[i]);
+      }
+      break;
+    case BRAKE:
+      for (i=0; i<4; i++) {
+        if (motor_speed[i] > BRAKE_ACCEL) {
+          motor_speed[i] -= BRAKE_ACCEL;
+        } else {
+          motor_speed[i] = 0;
+          state = STOP;
+          delay(200);
+        }
+        motor[i].setSpeed(motor_speed[i]);
+      }
+      break;
+    case AVOID:
+      avoid_obstacle();
+      break;
+  }
+
+  delay(10);
 
 }
